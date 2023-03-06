a76f5b2f754c6573b302b87a3f259a819a356b82
nullnumber1-va <artromworkmail@gmail.com>
2023-03-07 00:13:21 +0300

    Added corner cases handling

--- src/main/java/com/nullnumber1/task1/CosPowerSeries.java
+++ src/main/java/com/nullnumber1/task1/CosPowerSeries.java
@@ -4,6 +4,12 @@ public class CosPowerSeries {
 
     // cos(x) = 1 - x^2/2! + x^4/4! - x^6/6! + ... + (-1)^n * x^(2n) / (2n)!
     public static double calculate(double x, int n) {
+        if (Double.isNaN(x)) {
+            return Double.NaN;
+        } else if (Double.isInfinite(x)) {
+            return Double.NaN;
+        }
+
         double result = 0;
         for (int i = 0; i <= n; i++) {
             int sign = (i % 2 == 0) ? 1 : -1;
